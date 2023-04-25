78c5b04ca7a3141909e023af54099fe5b7bfbd1d
landuoduo <vv22345@163.com>
2023-04-25 12:20:42 +0800

    fix: vite配置文件

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,24 @@
-import { defineConfig } from 'vite'
+import { type ConfigEnv, type UserConfigExport, loadEnv } from "vite"
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
+console.log(loadEnv)
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-  ],
-})
+export default (configEnv: ConfigEnv): UserConfigExport => {
+  const viteEnv = loadEnv(configEnv.mode, process.cwd())
+  const { VITE_APP_BASE } = viteEnv
+  return {
+    base: VITE_APP_BASE,
+    plugins: [
+      vue(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
+  }
+}
